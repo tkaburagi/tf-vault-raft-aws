1a6d9f337e9c8ef6d256181fb3f2764d9cc0a7ea
Takayuki Kaburagi <t.kaburagi@me.com>
2020-05-23 22:34:30 +0900

    first commit

--- network.tf
+++ network.tf
@@ -58,7 +58,7 @@ resource "aws_route53_record" "vault" {
     allow_overwrite = true
     zone_id = aws_route53_zone.primary.id
     name    = "vault.${aws_route53_zone.primary.name}"
-    type    = "A"
+    type    = "CNAME"
     ttl     = "300"
     records = [
         aws_alb.vault_alb.dns_name
