180c2dc497627158ff09aa25116a86ab167cd1ec
Takayuki Kaburagi <t.kaburagi@me.com>
2020-05-25 14:24:00 +0900

    automate raft setup

--- raft-setup.sh
+++ raft-setup.sh
@@ -1,15 +1,44 @@
 #!/bin/bash
 
-PUB_IP_0=
-PUB_IP_1=
-PUB_IP_2=
+PUB_IP_0=$(
+  aws ec2 describe-addresses \
+        --filters "Name=tag-key,Values=Name" \
+                  "Name=tag-value,Values=kabu_vault_eip" \
+        | jq '.Addresses | sort_by(.PrivateIpAddress)' \
+        | jq -r '.[0].PublicIp'
+  )
+
+echo "PUB_IP_0: "${PUB_IP_0}
+
+PUB_IP_1=$(
+  aws ec2 describe-addresses \
+        --filters "Name=tag-key,Values=Name" \
+                  "Name=tag-value,Values=kabu_vault_eip" \
+        | jq '.Addresses | sort_by(.PrivateIpAddress)' \
+        | jq -r '.[1].PublicIp'
+  )
+
+echo "PUB_IP_1: "${PUB_IP_1}
+
+
+PUB_IP_2=$(
+  aws ec2 describe-addresses \
+        --filters "Name=tag-key,Values=Name" \
+                  "Name=tag-value,Values=kabu_vault_eip" \
+        | jq '.Addresses | sort_by(.PrivateIpAddress)' \
+        | jq -r '.[2].PublicIp'
+  )
+
+echo "PUB_IP_2: "${PUB_IP_2}
 
 # Init Learder
+echo "# Init Learder"
 ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_0} \
 -o "StrictHostKeyChecking no" \
-VAULT_ADDR=http://${PUB_IP_0}:8200 ./vault operator init
+VAULT_ADDR=http://${PUB_IP_0}:8200 ./vault operator init -format-json
 
 # Join Cluster
+echo "# Join Cluster"
 ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_1} \
 -o "StrictHostKeyChecking no" \
 VAULT_ADDR=http://${PUB_IP_1}:8200 ./vault operator raft join http://${PUB_IP_0}:8200
@@ -19,22 +48,17 @@ ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_2} \
 VAULT_ADDR=http://${PUB_IP_2}:8200 ./vault operator raft join http://${PUB_IP_0}:8200
 
 # Check Status
-echo "#### NODE1 STATUS"
+echo "#### NODE0 STATUS"
 ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_0} \
 -o "StrictHostKeyChecking no" \
 VAULT_ADDR=http://${PUB_IP_0}:8200 ./vault status
 
-echo "#### NODE2 STATUS"
+echo "#### NODE1 STATUS"
 ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_1} \
 -o "StrictHostKeyChecking no" \
 VAULT_ADDR=http://${PUB_IP_1}:8200 ./vault status
 
-echo "#### NODE3 STATUS"
+echo "#### NODE2 STATUS"
 ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_2} \
 -o "StrictHostKeyChecking no" \
 VAULT_ADDR=http://${PUB_IP_2}:8200 ./vault status
-
-echo "#### LIST RAFT PEERS"
-ssh -i ~/.ssh/hashistack.pem ubuntu@${PUB_IP_0} \
--o "StrictHostKeyChecking no" \
-VAULT_ADDR=http://${PUB_IP_0}:8200 ./vault operator raft list-peers
