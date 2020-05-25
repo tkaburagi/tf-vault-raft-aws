a4f31248c6c67dc35d1886184e53e5c847630d84
Takayuki Kaburagi <t.kaburagi@me.com>
2020-05-25 11:11:47 +0900

    raft

--- instance.tf
+++ instance.tf
@@ -13,6 +13,7 @@ resource "aws_instance" "vault_ec2" {
                 #!/bin/sh
 
                 cd /home/ubuntu
+                mkdir vault-raft-data
 
                 sudo apt-get install zip unzip
 
@@ -35,11 +36,11 @@ resource "aws_instance" "vault_ec2" {
                 sed "s|CLUSTER_ADDR_REPLACE|`echo $CLUSTER_ADDR_REPLACE`|g" config-0.hcl > config-1.hcl
                 sed "s|NODE_ID_REPLACE|`echo $CLUSTER_ADDR_REPLACE`|g" config-1.hcl > config.hcl
 
+                rm config-*.hcl
+
                 sleep 60
 
                 nohup ./vault server -config /home/ubuntu/config.hcl start -log-level=debug > vault.log &
-
-                ./vault operator init > .init.log
 
               EOF
 }
