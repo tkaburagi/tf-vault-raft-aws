790fe5091f2dc5dcfbebf22a233430b2cf2bd049
Takayuki Kaburagi <t.kaburagi@me.com>
2020-05-25 10:26:39 +0900

    raft

--- instance.tf
+++ instance.tf
@@ -33,7 +33,7 @@ resource "aws_instance" "vault_ec2" {
 
                 sed "s|API_ADDR_REPLACE|`echo $API_ADDR_REPLACE`|g" vault-tempate-aws.hcl > config-0.hcl
                 sed "s|CLUSTER_ADDR_REPLACE|`echo $CLUSTER_ADDR_REPLACE`|g" config-0.hcl > config-1.hcl
-                sed "s|NODE_ID_REPLACE|`echo CLUSTER_ADDR_REPLACE`|g" config-1.hcl > config.hcl
+                sed "s|NODE_ID_REPLACE|`echo $CLUSTER_ADDR_REPLACE`|g" config-1.hcl > config.hcl
 
                 sleep 60
 
